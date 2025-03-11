ef4334d03d2751bc4dcf45208fd1c91c420de188
830Utpal <lakshman2016yadav@gmail.com>
2025-03-12 00:36:44 +0530

    footer

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -9,6 +9,14 @@ const Footer = () => {
             <p className='text-gray-500 mb-4'>
                 Be the first to hear about new products, exclusive events, and online offers.
             </p>
+            <p>Sign Up and get 105 off on your first order.</p>
+            {/**newsletter form */}
+            <form className='flex'>
+              <input type="email" placeholder="Enter your email" className="p-3 w-full text-sm border-t border-l border-b border-gray-300 eounded-l-md focus:outline-none focus:ring-2 focus:ring-grey-500 transition-all"
+              reuired
+              />
+              <button type="submit" className='bg-black text-white px-6 py-3 text-sm rounded-r-md hover:bg-gray-800 transition-all'>Subscribe</button>
+            </form>
         </div>
       </div>
     </footer>
