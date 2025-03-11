0ceff50e6287c03a28121610c0ede7bdb394ecd5
830Utpal <lakshman2016yadav@gmail.com>
2025-03-12 00:42:01 +0530

    footer

--- frontend/src/components/Common/Footer.jsx
+++ frontend/src/components/Common/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -9,7 +10,7 @@ const Footer = () => {
             <p className='text-gray-500 mb-4'>
                 Be the first to hear about new products, exclusive events, and online offers.
             </p>
-            <p>Sign Up and get 105 off on your first order.</p>
+            <p className='font-medium text-sm text-gray-600 mb-6'>Sign Up and get 105 off on your first order.</p>
             {/**newsletter form */}
             <form className='flex'>
               <input type="email" placeholder="Enter your email" className="p-3 w-full text-sm border-t border-l border-b border-gray-300 eounded-l-md focus:outline-none focus:ring-2 focus:ring-grey-500 transition-all"
@@ -18,6 +19,30 @@ const Footer = () => {
               <button type="submit" className='bg-black text-white px-6 py-3 text-sm rounded-r-md hover:bg-gray-800 transition-all'>Subscribe</button>
             </form>
         </div>
+        {/**shop links */}
+        <h3 className='text-lg text-gray-800 mb-4'>Shop</h3>
+        <ul className='space-y-2 text-grey-600'>
+            <li>
+                <Link to="#" className='hover:text-gray-500 transition-colors'>
+                Men's Top wear
+                </Link>
+            </li>
+            <li>
+                <Link to="#" className='hover:text-gray-500 transition-colors'>
+                Women's Top wear
+                </Link>
+            </li>
+            <li>
+                <Link to="#" className='hover:text-gray-500 transition-colors'>
+                Men's Bottom wear
+                </Link>
+            </li>
+            <li>
+                <Link to="#" className='hover:text-gray-500 transition-colors'>
+                Women's Bottom wear
+                </Link>
+            </li>
+        </ul>
       </div>
     </footer>
   )
