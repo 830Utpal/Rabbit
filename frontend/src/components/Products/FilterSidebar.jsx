20306b9e6f9f199cfd057a9329dcd265d4b8b9f7
830Utpal <lakshman2016yadav@gmail.com>
2025-03-13 03:47:20 +0530

    handle filter change

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -71,7 +71,24 @@ const FilterSidebar = () => {
         })
         setPriceRange([0, params.maxPrice || 100]);
     
-    },[searchParams])
+    },[searchParams]);
+
+    const handleFilterChange=(e)=>{
+      const{name,value,checked,type}=e.target;
+      let newFilters={...filters};
+      if(type==="checkbox"){
+            if(checked){
+                newFilters[name]=[...(newFilters[name] || []),value];
+            }else{
+                newFilters[name]=newFilters[name].filter((item)=> item!==value);
+            }
+        }else{
+            newFilters[name]=value;
+        }
+        setFilters(newFilters);
+      };
+
+     
 
 
 
@@ -83,7 +100,8 @@ const FilterSidebar = () => {
         <label className='block text-gray-600 font- medium mb-2'>Category</label>
         {categories.map((category)=>(
             <div key={category} className='flex items-center mb-1'>
-                <input type="radio" name="category" className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300'/>
+                <input type="radio" name="category"
+                value={category} onChange={handleFilterChange} className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300'/>
                 <span className="text-gray-700">{category}</span>
             </div>
         ))}
@@ -94,7 +112,8 @@ const FilterSidebar = () => {
         <label className='block text-gray-600 font- medium mb-2'>Gender</label>
         {genders.map((gender)=>(
             <div key={gender} className='flex items-center mb-1'>
-                <input type="radio" name="gender" className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300'/>
+                <input type="radio" name="gender" 
+                value={gender} onChange={handleFilterChange} className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300'/>
                 <span className="text-gray-700">{gender}</span>
             </div>
         ))}
@@ -105,7 +124,9 @@ const FilterSidebar = () => {
         <label className="block text-gray-600 font-medium mb-2">Color</label>
         <div className='flex flex-wrap gap-2'>
             {colors.map((color)=>(
-                <button key={color} name="color" className='w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105' style={{backgroundColor:color.toLowerCase()}}></button>
+                <button key={color} name="color"
+                value={color}
+                onClick={handleFilterChange} className='w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105' style={{backgroundColor:color.toLowerCase()}}></button>
             ))}
         </div>
     </div>
@@ -118,6 +139,8 @@ const FilterSidebar = () => {
             <input 
                 type="checkbox" 
                 name="size" 
+                value={size}
+                onChange={handleFilterChange}
                 className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300' 
             />  
             <span className='text-gray-700'>{size}</span> 
@@ -134,6 +157,8 @@ const FilterSidebar = () => {
             <input 
                 type="checkbox" 
                 name="material" 
+                value={material}
+                onChange={handleFilterChange}
                 className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300' 
             />  
             <span className='text-gray-700'>{material}</span> 
@@ -150,6 +175,8 @@ const FilterSidebar = () => {
             <input 
                 type="checkbox" 
                 name="brand" 
+                value={brand}
+                onChange={handleFilterChange}
                 className='mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300' 
             />  
             <span className='text-gray-700'>{brand}</span> 
