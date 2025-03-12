1f63960d9de2bf225b5a54e4eaed880464e44fb2
830Utpal <lakshman2016yadav@gmail.com>
2025-03-12 18:44:21 +0530

    productGrid like section

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import {  useEffect, useState } from "react";
 import {toast} from "sonner";
+import ProductGrid from "./ProductGrid";
 
 const selectedProduct = {
     name: "Stylish jacket",
@@ -21,6 +22,33 @@ const selectedProduct = {
       },
     ],
   };
+
+  const similarProducts=[
+    {
+        _id:1,
+        name:"Product 1",
+        price:100,
+        images:[{url:"https://picsum.photos/500/500?random=3"}]
+    },
+    {
+        _id:1,
+        name:"Product 2",
+        price:100,
+        images:[{url:"https://picsum.photos/500/500?random=4"}]
+    },
+    {
+        _id:1,
+        name:"Product 3",
+        price:100,
+        images:[{url:"https://picsum.photos/500/500?random=5"}]
+    },
+    {
+        _id:1,
+        name:"Product 4",
+        price:100,
+        images:[{url:"https://picsum.photos/500/500?random=2"}]
+    }
+  ]
   
   const ProductDetails = () => {
     const[mainImage,setMainImage]=useState("");
@@ -172,6 +200,10 @@ const selectedProduct = {
                 </table>
               </div>
             </div>
+          </div>
+          <div className="mt=20">
+            <h2 className="text-2xl text-center font-medium mb-4 pt-20 pb-7">You May Also Like</h2>
+            <ProductGrid products={similarProducts}/>
           </div>
         </div>
       </div>
