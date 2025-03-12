9eb25bd5db450e3b9ef404ebec9fb1590e04c1a4
830Utpal <lakshman2016yadav@gmail.com>
2025-03-12 23:31:30 +0530

    profile pafe finished frontend with allign change

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -46,34 +46,34 @@ useEffect(()=>{
         <table className='min-w-full text-left text-gray-500'>
             <thead className="bg-gray-100 text-xs uppercase text-gray-700">
                 <tr>
-                    <th className="py-2 sm:py-3">Image</th>
-                    <th className="py-2 sm:py-3">Order ID</th>
-                    <th className="py-2 sm:py-3">Created</th>
-                    <th className="py-2 sm:py-3">Shipping Address</th>
-                    <th className="py-2 sm:py-3">Items</th>
-                    <th className="py-2 sm:py-3">Price</th>
-                    <th className="py-2 sm:py-3">Status</th>
+                    <th className="py-3 px-4">Image</th>
+                    <th className="py-3 px-4">Order ID</th>
+                    <th className="py-3 px-4">Created</th>
+                    <th className="py-3 px-4">Shipping Address</th>
+                    <th className="py-3 px-4">Items</th>
+                    <th className="py-3 px-4">Price</th>
+                    <th className="py-3 px-4">Status</th>
                 </tr>
             </thead>
             <tbody>
                 {orders.length>0?(
                     orders.map((order)=>(
                         <tr key={order._id} className='border-b hover:border-gray-50 cursor-pointer'>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                            <td className='py-3 px-4'>
                               <img src={order.orderItems[0].image}
                               alt={order.orderItems[0].name} className='w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-lg'/>
                             </td>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4 font-medium text-gray-900 whitespace-nowrap'>#{order._id}</td>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                            <td className='py-3 px-4 font-medium text-gray-900 whitespace-nowrap'>#{order._id}</td>
+                            <td className='py-3 px-4'>
                                 {new Date(order.createdAt).toLocaleDateString()}{" "}
                                 {new Date(order.createdAt).toLocaleTimeString()}
                             </td>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                            <td className='py-3 px-4'>
                                 {order.shippingAddress ? `${order.shippingAddress.city},${order.shippingAddress.country}`:"N/A"}
                             </td>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4'>{order.orderItems.length}</td>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4'>{order.totalPrice}</td>
-                            <td className='py-2 px-2 sm:py-4 sm:px-4'>
+                            <td className='py-3 px-4'>{order.orderItems.length}</td>
+                            <td className='py-3 px-4'>{order.totalPrice}</td>
+                            <td className='py-3 px-4'>
                                 <span className={`${order.isPaid ? "bg-green-100 text-green-700":"bg-red-100 text-red-700"}
                                 px-2 py-1 rounded-full text-xs sm:text-sm font-medium`}>{order.isPaid ? "Paid" : "Pending"}</span>
                             </td>
