641f39e9493b6d88681081e4a57eba7d49e70d4d
830Utpal <lakshman2016yadav@gmail.com>
2025-03-26 17:00:01 +0530

    admin user management

--- frontend/src/components/Admin/UserManagement.jsx
+++ frontend/src/components/Admin/UserManagement.jsx
@@ -4,6 +4,7 @@ const UserManagement = () => {
 
     const users=[
         {
+            _id:"123213",
           name:"John Doe",
           email:"john@example.com",
           role:"admin",
@@ -38,6 +39,12 @@ const UserManagement = () => {
 
     const handleRoleChange=(userId,newRole)=>{
         console.log({id:userId,role:newRole})
+    }
+
+    const handleDeleteUser=(userId)=>{
+        if(window.confirm("Are you sure you want to delete this user")){
+            console.log("deleting user with ID",userId)
+        }
     }
   return (
     <div className='max-w-7xl mx-auto p-6'>
@@ -93,7 +100,11 @@ const UserManagement = () => {
                             <option value="admin">Admin</option>
                         </select>
                     </td>
-                    <td className='p-4'>{user.email}</td>
+                    <td className='p-4'>
+                        <button onClick={()=>handleDeleteUser(user._id)}className='bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600'>
+                            Delete
+                        </button>
+                    </td>
                 </tr>
                ))}
            </tbody>
