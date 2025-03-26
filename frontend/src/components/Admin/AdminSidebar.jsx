eb974761c07204f29233f4d12f24bbb0339922a5
830Utpal <lakshman2016yadav@gmail.com>
2025-03-26 12:36:21 +0530

    admin

--- frontend/src/components/Admin/AdminSidebar.jsx
+++ frontend/src/components/Admin/AdminSidebar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { FaUser } from 'react-icons/fa'
-import { Link, NavLink } from 'react-router-dom'
+import { FaBoxOpen, FaClipboardList, FaSignOutAlt, FaStore, FaUser } from 'react-icons/fa'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 const AdminSidebar = () => {
+
+
+    const navigate=useNavigate();
+    const handleLogout=()=>{
+       navigate("/");
+    }
   return (
     <div className='p-6'>
      <div className="mb-6">
@@ -14,7 +20,25 @@ const AdminSidebar = () => {
         <FaUser/>
         <span>Users</span>
        </NavLink>
+       <NavLink to="/admin/products" className={({isActive})=>isActive?"bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2":"text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"}>
+        <FaBoxOpen/>
+        <span>Products</span>
+       </NavLink>
+       <NavLink to="/admin/orders" className={({isActive})=>isActive?"bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2":"text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"}>
+        <FaClipboardList/>
+        <span>Orders</span>
+       </NavLink>
+       <NavLink to="/" className={({isActive})=>isActive?"bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2":"text-gray-300 hover:bg-gray-700 hover:text-white py-3 px-4 rounded flex items-center space-x-2"}>
+        <FaStore/>
+        <span>Shop</span>
+       </NavLink>
      </nav>
+     <div className="mt-6">
+        <button onClick={handleLogout} className='w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded flex items-center justify-center space-x-2'>
+           <FaSignOutAlt/>
+           <span>Logout</span>
+        </button>
+     </div>
     </div>
   )
 }
