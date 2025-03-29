f9532e187bf4920cc4f5f694b5e081e3db89c027
830Utpal <lakshman2016yadav@gmail.com>
2025-03-29 10:35:09 +0530

    login  api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -49,5 +49,47 @@ router.post("/register",async(req,res)=>{
 //@desc authenticate
 //@access public
 
+router.post("/login",async(req,res)=>{
+  const{email,password}=req.body;
+  try{
+     let user =await User.findOne({email});
+
+     if(!user)return res.status(400).json({message:"Invalid Credentials"});
+     const isMatch= await user.matchPassword(password);
+
+     if(!isMatch) return res.status(400).json({message:"Invalid Credentials"})
+
+           //create jwt payload
+           const payload ={user:{id:user._id,role:user.role}};
+
+           //sign and return the token along with user data
+     
+           jwt.sign(payload,process.env.JWT_SECRET,{expiresIn:"40h"},(err,token)=>{
+             if(err) throw err;
+     
+             //send the user and token in response
+     
+             res.json({
+               user:{
+                 _id:user._id,
+                 name:user.name,
+                 email:user.email,
+                 role:user.role
+               },token,
+             })
+           });
+  }catch(error){
+     console.log(error);
+     res.status(500).send("Server Error")
+  }
+})
+
+//@route GET/api/users/profile
+//@desc get logged in users profile(protected route)
+//private access
+
+router.get("/profile",async(req,res)=>{
+  res.json(req.user);
+})
 
 module.exports=router;
