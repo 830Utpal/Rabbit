a755303891413319909ae14c97a9acffb6fb0c88
830Utpal <lakshman2016yadav@gmail.com>
2025-06-18 22:33:06 +0530

    carts routes and schema

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -59,10 +59,47 @@ router.post('/', async (req, res) => {
                 }],
                 totalPrice: product.price * quantity,
             });
+            await newCart.save(); 
             return res.status(201).json(newCart);
         }
     } catch (error) {
         console.error("Error adding to cart:", error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// @route PUT /api/cart
+// @desc Update a product quantity in the cart for a guest or logged-in user
+// @access Public
+
+router.put('/', async (req, res) => {
+     const { productId, quantity, size, color, guestId, userId } = req.body;
+    try {
+        let cart = await getCart(userId, guestId);
+
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+
+        const productIndex = cart.products.findIndex((p) => p.productId.toString() === productId && p.size === size && p.color === color);
+        
+        if (productIndex > -1) {
+            // Update quantity
+            if (quantity <= 0) {
+                // Remove item if quantity is 0 or less
+                cart.products.splice(productIndex, 1);
+            } else {
+                cart.products[productIndex].quantity = quantity;
+            }
+            // Recalculate total price
+            cart.totalPrice = cart.products.reduce((acc, item) => acc + item.price * item.quantity, 0);
+            await cart.save();
+            res.status(200).json(cart);
+        } else {
+            res.status(404).json({ message: 'Product not found in cart' });
+        }
+    } catch (error) {
+        console.error("Error updating cart:", error);
         res.status(500).json({ message: 'Server error' });
     }
 });
