3546edd5b6bd95ecd09e79e83738c18b41b70def
830Utpal <lakshman2016yadav@gmail.com>
2025-06-19 20:29:03 +0530

    checkout routes

--- backend/routes/checkoutRoutes.js
+++ backend/routes/checkoutRoutes.js
@@ -5,6 +5,8 @@ const Product = require('../models/Product');
 const { protect } = require('../middleware/authMiddleware');
 const Order = require('../models/Order');
 
+const router = express.Router();
+
 // @route POST /api/checkout
 // @desc Create a new checkout
 // @access Private
@@ -35,4 +37,31 @@ router.post('/', protect, async (req, res) => {
     }
 });
 
+// @route GET /api/checkout/:id/pay
+// @desc Update checkout payment status
+// @access Private
 
+router,put('/:id/pay', protect, async (req, res) => {
+    const {paymentStatus,paymentDetails} = req.body;
+
+    try {
+        const checkout = await Checkout.findById(req.params.id);
+        if (!checkout) {
+            return res.status(404).json({message: "Checkout not found"});
+        }
+
+        if(paymentStatus=== 'Paid') {
+            checkout.isPaid = true;
+            checkout.paymentStatus = paymentStatus;
+            checkout.paymentDetails = paymentDetails;
+            checkout.paidAt = Date.now();
+            await checkout.save();
+            res.status(200).json(checkout);
+        } else {
+            res.status(400).json({message: "Invalid payment status"});
+        }
+    } catch (error) {
+        console.error("Error updating checkout payment status:", error);
+        res.status(500).json({message: "Server error"});
+    }
+});
