396b0b8652ad3c7dae60a7194b13a605ed0aef80
830Utpal <lakshman2016yadav@gmail.com>
2025-06-20 17:23:59 +0530

    upload  routes

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+
+require('dotenv').config();
+
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Set up multer for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single("file"), (req, res) => {
+    try {
+        if(!req.file) {
+            return res.status(400).json({ message: "No file uploaded" });
+        }
+        // function to handle the streaming upload to Cloudinary
+        const streamUpload = (fileBuffer) => {
+            return new Promise((resolve, reject) => {
+                const stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+                streamifier.createReadStream(fileBuffer).pipe(stream);
+            });
+        };
+        //call the streamUpload function with the uploaded file buffer
+        const result = streamUpload(req.file.buffer);
+        res.json({imageUrl: result.secure_url, message: "File uploaded successfully"});
+    } catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+});
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const userRoutes=require("./routes/userRoutes");
 const productRoutes=require("./routes/productRoutes");
 const cartRoutes=require("./routes/cartRoutes");
 const checkoutRoutes=require("./routes/checkoutRoutes");
+const orderRoutes=require("./routes/orderRoutes");
+const uploadRoutes=require("./routes/uploadRoutes");
+
 
 const app=express();
 app.use(express.json());
@@ -29,6 +32,8 @@ app.use("/api/users",userRoutes);
 app.use("/api/products",productRoutes);
 app.use("/api/cart",cartRoutes);
 app.use("/api/checkout",checkoutRoutes);
+app.use("/api/orders",orderRoutes);
+app.use("/api/upload",uploadRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`);
